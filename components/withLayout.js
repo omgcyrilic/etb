10ba01829d8c360d6f329ee27a6ef0fea4c040cf
Brian Parks <cyrilic@gmail.com>
2018-10-31 09:32:30 -0500

    faveico thangs

--- components/withLayout.js
+++ components/withLayout.js
@@ -96,7 +96,8 @@ const withLayout = (WrappedComponent) => {
 
       return (
         <div>
-          <Favicon url="https://images.eatthisbeef.com/faveico.png" />
+          <Favicon url="https://images.eatthisbeef.com/favicon.png" />
+          <Favicon url="https://images.eatthisbeef.com/favicon.ico" />
           <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
             <meta name="description" content="DFW's most epic burger ranking publication of all time.  Feed your life-long burger quest in Dallas, TX." />
